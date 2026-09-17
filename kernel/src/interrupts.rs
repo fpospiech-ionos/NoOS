943a6fbf58abb4d1a959f7da2b9408d5d130bcab
fpospiech <fpospiech@united-internet.de>
2026-09-17 10:19:11 +0200

    add page fault handler and gp handler

--- kernel/src/interrupts.rs
+++ kernel/src/interrupts.rs
@@ -1,14 +1,15 @@
 use lazy_static::lazy_static;
-use x86_64::{structures::idt::{InterruptDescriptorTable, InterruptStackFrame}};
+use x86_64::structures::{idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode}, paging::page};
 
 use crate::{gdt::DOUBLE_FAULT_IST_INDEX, println};
-
 
 lazy_static!{
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
 
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt.page_fault.set_handler_fn(page_fault_handler);
+        idt.general_protection_fault.set_handler_fn(general_protection_fault_handler);
 
         unsafe {
             idt.double_fault.set_handler_fn(double_fault_handler).set_stack_index(DOUBLE_FAULT_IST_INDEX);
@@ -17,8 +18,9 @@ lazy_static!{
     };
 }
 
-pub fn init_idt() {
-    IDT.load();
+enum Interrupt {
+    Timer = 32,
+    Keyboard,
 }
 
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
@@ -27,4 +29,18 @@ extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
 
 extern "x86-interrupt" fn double_fault_handler(stack_frame: InterruptStackFrame, error_code: u64) -> ! {
     panic!("[EXCEPTION] (double fault: {:?}) {:?}", error_code, stack_frame)
+}
+
+extern "x86-interrupt" fn page_fault_handler(stack_frame: InterruptStackFrame, code: PageFaultErrorCode) {
+    println!("[PAGE FAULT Code] {:?}", code);
+    println!("[PAGE FAULT Stack] {:?}", stack_frame);
+}
+
+extern "x86-interrupt" fn general_protection_fault_handler(stack_frame: InterruptStackFrame, code: u64) {
+    println!("[GP Code] {:?}", code);
+    println!("[GP Stack] {:?}", stack_frame);
+}
+
+pub fn init_idt() {
+    IDT.load();
 }
